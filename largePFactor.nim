6b109cde42504b5c239fa0f799fce4f4d333b718
Luís Figueiredo <luiscfet@gmail.com>
2017-01-29 01:53:57 +0000

    add ex12 removed unnecessary code from largePFactor

--- largePFactor.nim
+++ largePFactor.nim
@@ -12,29 +12,15 @@ proc primeFac(n: int64): seq[int64] =
   var d = 1
   var q: int64 = (n %% 2) and 2 or 3    # either 2 or 3, alternating
   while (q <= maxq) and ((n %% q) != 0):
-      q = getStep(d)
-      d += 1
+    q = getStep(d)
+    d += 1
   if q <= maxq:        
-      var q1: seq[int64] = primeFac(n /% q)
-      var q2: seq[int64] = primeFac(q)
-      res = concat(q2, q1, res)
+    var q1: seq[int64] = primeFac(n /% q)
+    var q2: seq[int64] = primeFac(q)
+    res = concat(q2, q1, res)
   else: 
-      res.add(n)    
+    res.add(n)    
   result = res
- 
-var is_prime: seq[bool] = @[]
-is_prime.add(false)
-is_prime.add(false)
- 
-iterator primes(limit: int): int =
-  for n in high(is_prime)..limit+2: is_prime.add(true)    
-  for n in 2..limit + 1:
-    if is_prime[n]:
-      yield n
-      for i in countup((n *% n), limit+1, n): # start at ``n`` squared
-        try:
-          is_prime[i] = false
-        except IndexError: break
 
 var f = primeFac(600851475143)
 echo f
